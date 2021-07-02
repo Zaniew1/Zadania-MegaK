517badaabaa82a138dfdbc051a3a9ae0eece4c29
Mateusz Zaniewski <67514879+Zaniew1@users.noreply.github.com>
2021-07-02 12:39:36 +0200

    Add files via upload

---
+++ Pierwsze zadanie megaK.js
@@ -0,0 +1,20 @@
+const div = document.createElement('div');
+document.body.appendChild(div);
+    document.body.style.height = '100vh'
+    div.style.display = 'flex';
+    div.style.height = '100%';
+    div.style.justifyContent = 'center';
+    div.style.alignItems = 'center';
+    div.style.fontSize = '40px';
+const name = prompt("Podaj swoje imię ");
+const surname = prompt("Podaj swoje Nazwisko ");
+const age = prompt("Podaj swój wiek ");
+    
+   
+if(age >= 18){
+    
+    div.textContent = `Masz na imię ${name} ${surname}`;
+    console.log(div);
+} else {
+    div.textContent = `Jesteś niepełnoletni`
+}
